5090aedd953025d47e3ddc7e8bca8be805fdfd2a
IvanVijandi <ivijandi75@gmail.com>
2025-10-26 20:03:47 -0300

    get_query_embedding added

--- Langchain/app/core/embbedings.py
+++ Langchain/app/core/embbedings.py
@@ -14,5 +14,13 @@ def get_document_embedding(text: list[str]) -> list[list[float]]:
     ##SEMANTIC_SIMILARITY : incrustaciones optimizadas para evaluar la similitud del texto.
 
 
-embedding = get_document_embedding("Hello, world!")
-print(embedding[0])
+def get_query_embedding(text: str) -> list[float]:
+    
+    return embeddings.embed_query(text=text, task_type="SEMANTIC_SIMILARITY", output_dimensionality=128)
+
+
+doc_embedding = get_document_embedding("Hello, world!")
+print(f"doc_embedding:",doc_embedding[0])
+
+query_embedding = get_query_embedding("Hello, world!")
+print(f"query_embedding:",query_embedding)
