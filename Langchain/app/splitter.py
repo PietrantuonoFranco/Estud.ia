918dfbb16c91fa63efa3d03c11fd9bc16adfd079
IvanVijandi <ivijandi75@gmail.com>
2025-10-30 15:59:49 -0300

    refactor splitter

--- Langchain/app/splitter.py
+++ Langchain/app/splitter.py
@@ -3,28 +3,31 @@ from langchain_community.document_loaders import PyPDFLoader
 
 
 ##Función para generar chunks de texto
-def splitter(file_path:str)->list[str]:
+def splitter_CharacterTextSplitter(file_path:str):
     
-    """Divide un texto en fragmentos más pequeños utilizando RecursiveCharacterTextSplitter."""
+    """Divide un texto en Document, donde cada uno es una pagina del pdf. Luego divide cada Document en chunks de texto más pequeños.
+        Retorna una lista de documentos (chunks)
+    """
     
     if file_path:
         loader = PyPDFLoader(file_path)
         docs = loader.load() ##Crea un documento por pagina del PDF
     
     text_splitter = RecursiveCharacterTextSplitter( ##Recibe solo objeto del tipo Document
-        chunk_size=1000,  # Tamaño del chunk (caracteres)
+        chunk_size=1500,  # 1500 caracteres por chunk, aprox. seguro para 2,048 tokens del modelo Gemini que usamos en embeddings
         chunk_overlap=200,  
         add_start_index=True,  
+        separators=["\n\n", "\n", ". ", "? ", "! ", " ", ""] ##Crea un nuevo chunk en el primer separador que encuentre
     )
     
-    all_splits = text_splitter.split_documents(docs)
+    all_splits = text_splitter.split_documents(docs) ##Versione futuras, se podría tokenizar el texto !
     
-    chunks: list[str] = [];
+    return all_splits
     
-    for i in all_splits:
-        chunks.append(i.page_content)
-    return chunks
+
+
 
 ##Ejemplo de uso con PDF
-chunks = splitter(file_path = "C:/Users/ivija/Desktop/Estud.ia/Langchain/app/bitcoin_es.pdf")
-print(chunks[0])##Imprime el contenido del primer chunk
+path = "./bitcoin_es.pdf"
+chunks = splitter_CharacterTextSplitter(file_path = path)
+print(chunks[7])##Imprime el contenido del primer chunk
