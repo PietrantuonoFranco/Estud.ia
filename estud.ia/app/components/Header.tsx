e13228277ad2652fad982e89943c86f8ab73d640
Franquinios <francopietrantuono999@gmail.com>
2025-12-21 20:28:55 -0300

    feat: add Home Header

--- estud.ia/app/components/Header.tsx
+++ estud.ia/app/components/Header.tsx
@@ -1,16 +1,9 @@
 "use client"
 
-import { Share2, Settings} from "lucide-react"
+import { Settings } from "lucide-react"
 import Image from "next/image"
 
 export default function Header() {
-  const handleShare = async () => {
-    try {
-      console.log("Compartir")
-    } catch (error) {
-      console.error(error)
-    }
-  }
 
   const handleSettings = async () => {
     try {
@@ -26,24 +19,17 @@ export default function Header() {
         <div className="flex h-9 w-9 items-center justify-center rounded-lg">
           <Image  src="/favicon.png" alt="Estud.IA Logo" width={32} height={32} className="rounded-full" />
         </div>
-        <h1 className="text-lg font-medium text-foreground">University Guide to Legislation and Civil Law</h1>
+        <h1 className="text-lg font-medium text-foreground">Estud.IA</h1>
       </div>
 
       <div className="flex items-center gap-3">
         <button
           type="button"
-          onClick={handleShare}
-          className="cursor-pointer text-muted-foreground rounded-full p-2 bg-card hover:bg-[var(--hover-bg)]"
-        >
-          <Share2 className="h-4 w-4" />
-        </button>
-
-        <button
-          type="button"
           onClick={handleSettings}          
-          className="cursor-pointer text-muted-foreground rounded-full p-2 bg-card hover:bg-[var(--hover-bg)]"
+          className="cursor-pointer flex items-center gap-2 text-muted-foreground rounded-full py-2 px-4 bg-card hover:bg-[var(--hover-bg)]"
         >
           <Settings className="h-4 w-4" />
+          <span>Configuración</span>
         </button>
         
         <div className="h-8 w-8">
