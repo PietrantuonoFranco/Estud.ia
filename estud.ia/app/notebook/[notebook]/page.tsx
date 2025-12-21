2201397233cdea3e78f48ea53b7ff979cc9ed5ad
Franquinios <francopietrantuono999@gmail.com>
2025-12-21 20:16:24 -0300

    refactor: notebook

--- estud.ia/app/notebook/[notebook]/page.tsx
+++ estud.ia/app/notebook/[notebook]/page.tsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header";
+import SourcesPanel from "./components/SourcesPanel";
+import ChatPanel from "./components/Chat/ChatPanel";
+import StudioPanel from "./components/FunctionPanel";
 
 export default function Home() {
   return (
     <div className="flex h-screen flex-col bg-background">
       <Header />
       <div className="flex flex-1 overflow-hidden">
-        
+        <SourcesPanel />
+        <ChatPanel />
+        <StudioPanel />
       </div>
     </div>
   )
