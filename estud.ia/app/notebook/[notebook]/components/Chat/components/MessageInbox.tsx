4d148eb76ee446d3d2cf581dcbe483b76c603c26
Franquinios <francopietrantuono999@gmail.com>
2025-12-22 00:40:06 -0300

    feat: update MessageInbox component for improved styling and icon usage

--- estud.ia/app/notebook/[notebook]/components/Chat/components/MessageInbox.tsx
+++ estud.ia/app/notebook/[notebook]/components/Chat/components/MessageInbox.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Send } from "lucide-react"
+import { SendHorizontal } from "lucide-react"
 
 export default function MessageInbox () {
   const [message, setMessage] = useState("");
@@ -14,7 +14,7 @@ export default function MessageInbox () {
     }
   } 
   return (
-    <form onSubmit={handleSubmit} className="sticky -bottom-8 w-full flex items-center gap-3 rounded-lg border border-border bg-card text-sm leading-relaxed text-foreground px-4 py-2">
+    <form onSubmit={handleSubmit} className="mx-6 sticky -bottom-6  flex items-center gap-3 rounded-full border border-border bg-card text-sm leading-relaxed text-foreground pl-6 pr-1 py-1">
       <input
         type="text"
         placeholder="Comienza a escribir..."
@@ -25,9 +25,9 @@ export default function MessageInbox () {
 
       <button
         type="submit"
-        className="flex items-center justify-center h-8 w-8 shrink-0 rounded-md bg-muted-foreground"
+        className="cursor-pointer flex items-center justify-center h-10 w-10 shrink-0 rounded-full bg-muted-foreground text-card hover:bg-[var(--hover-bg)] hover:text-foreground transition-colors duration-300"
       >
-        <Send className="h-4 w-4" />
+        <SendHorizontal className="h-5 w-5" />
       </button>
     </form>
   )
