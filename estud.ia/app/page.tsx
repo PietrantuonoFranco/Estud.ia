8049b3d86dfbea5f6601ba49e05c326848535ee6
Franquinios <francopietrantuono999@gmail.com>
2025-12-21 22:28:20 -0300

    feat: restructure Home component layout to include OptionsBanner and NotebooksContainer

--- estud.ia/app/page.tsx
+++ estud.ia/app/page.tsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header";
+import OptionsBanner from "./components/OptionsBanner";
+import NotebooksContainer from "./components/NotebooksContainer";
+
 
 export default function Home() {
   return (
     <div className="flex h-screen flex-col bg-background">
       <Header />
-      <div className="flex flex-1 overflow-hidden">
-        
+      <div className="flex flex-col flex-1 overflow-hidden py-6 px-42 space-y-6">
+        <OptionsBanner />
+
+        <NotebooksContainer />
       </div>
     </div>
   )
