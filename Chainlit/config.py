1974c549c8540c51d81e82301110990113970e89
Franquinios <francopietrantuono999@gmail.com>
2025-12-22 13:19:34 -0300

    feat: add environment variables config

--- Chainlit/config.py
+++ Chainlit/config.py
@@ -1,8 +1,14 @@
 from pydantic_settings import BaseSettings
-
+import os
 
 class Settings(BaseSettings):
-    GOOGLE_API_KEY: str
+    DB_USER = str(os.getenv("POSTGRES_USER", "usuario_por_defecto"))
+    DB_PASSWORD = str(os.getenv("POSTGRES_PASSWORD", "password_por_defecto"))
+    DB_NAME = str(os.getenv("POSTGRES_DB", "nombre_db"))
+    DB_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
+
+    # Si la API está en Docker, el host es 'postgres'. Si es local, es 'localhost'.
+    DB_HOST = str(os.getenv("POSTGRES_HOST", "postgres")) 
 
 
 conf = Settings()
