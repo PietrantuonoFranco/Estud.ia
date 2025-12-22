3d566b903a837ff05f57fbf5e277fd496236ad4f
Franquinios <francopietrantuono999@gmail.com>
2025-12-21 22:28:14 -0300

    feat: add NotebooksContainer component and mock data for notebook display

---
+++ estud.ia/app/components/NotebooksContainer.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { MoreVertical, Plus } from "lucide-react";
+import { useState } from "react"
+
+import notebooksData from "./mocks/notebooksData.json"
+
+interface Notebook {
+    id: number
+    title: string
+    source: string
+    icon: string
+    date: Date
+    sourcesCount: number
+}
+
+
+export default function NotebooksContainer () {
+  const [notebooks, setNotebooks] = useState<Notebook[]>([]);
+
+  return (
+    <div className="w-full grid grid-cols-4 gap-4">
+      <div className="group cursor-pointer w-full h-48 space-y-2 flex flex-col justify-center items-center border-2 border-dashed border-border rounded-xl hover:bg-card transition-colors duration-300">
+        <div className="flex justify-center items-center p-2 rounded-full bg-card group-hover:bg-card/80">
+          <Plus className="w-8 h-8" />
+        </div>
+        <h3 className="mb-2 text-lg font-medium text-foreground">Crear cuaderno</h3>
+      </div>
+
+      {notebooksData.map((notebook, index) => (
+        <div
+          key={notebook.id}
+          className="group relative flex h-48 cursor-pointer flex-col justify-between rounded-xl p-4 bg-gradient-to-bl from-transparent to-purple-700/15 transition-colors hover:bg-card/80"
+        >
+          <div className="flex items-start justify-between">
+            <div className="flex h-12 w-12 items-center justify-center rounded-lg bg-muted text-2xl">
+              {notebook.icon}
+            </div>
+            <button
+              type="button"
+              className="h-8 w-8 opacity-0 transition-opacity group-hover:opacity-100"
+            >
+              <MoreVertical className="h-4 w-4" />
+            </button>
+          </div>
+          <div>
+            <h3 className="mb-2 text-lg font-medium text-foreground">{notebook.title}</h3>
+            <p className="text-sm text-foreground">
+              {notebook.sourcesCount} fuente · {notebook.date}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
