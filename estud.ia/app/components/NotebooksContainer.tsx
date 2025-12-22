3e0a47116c8cd072895526c024c2c0f3d136bdcd
Franquinios <francopietrantuono999@gmail.com>
2025-12-22 01:28:00 -0300

    feat: refactor NotebooksContainer to use form for file upload and improve structure

--- estud.ia/app/components/NotebooksContainer.tsx
+++ estud.ia/app/components/NotebooksContainer.tsx
@@ -6,26 +6,39 @@ import { useState } from "react"
 import notebooksData from "./mocks/notebooksData.json"
 
 interface Notebook {
-    id: number
-    title: string
-    source: string
-    icon: string
-    date: Date
-    sourcesCount: number
+  id: number
+  title: string
+  source: string
+  icon: string
+  date: Date
+  sourcesCount: number
 }
 
 
 export default function NotebooksContainer () {
   const [notebooks, setNotebooks] = useState<Notebook[]>([]);
 
+  const handleSubmit = () => {
+    
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    
+  };
+
   return (
     <div className="w-full grid grid-cols-4 gap-4">
-      <div className="group cursor-pointer w-full h-48 space-y-2 flex flex-col justify-center items-center border-2 border-dashed border-border rounded-xl hover:bg-card transition-colors duration-300">
-        <div className="flex justify-center items-center p-2 rounded-full bg-card group-hover:bg-card/80">
-          <Plus className="w-8 h-8" />
-        </div>
-        <h3 className="mb-2 text-lg font-medium text-foreground">Crear cuaderno</h3>
-      </div>
+      <form onSubmit={handleSubmit} className="group cursor-pointer w-full h-48 space-y-2 flex flex-col justify-center items-center border-2 border-dashed border-border rounded-xl hover:bg-card transition-colors duration-300">
+        <input type="file" accept="application/pdf" className="hidden" id="file-upload" onChange={handleFileChange} />
+
+        <label htmlFor="file-upload" className="cursor-pointer h-full w-full flex flex-col items-center justify-center">
+          <div className="flex justify-center items-center p-2 rounded-full bg-card group-hover:bg-card/80">
+            <Plus className="w-8 h-8" />
+          </div>
+          <h3 className="mb-2 text-lg font-medium text-foreground">Crear cuaderno</h3>
+        </label>
+      </form>
 
       {notebooksData.map((notebook, index) => (
         <div
