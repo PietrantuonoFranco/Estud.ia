3544d9e3cdd290ee3eebab1e60ef7989da45d818
Franquinios <francopietrantuono999@gmail.com>
2025-12-21 21:32:17 -0300

    feat: add OptionsBanner component for enhanced UI options

---
+++ estud.ia/app/components/OptionsBanner.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { Check, LayoutGrid, StretchHorizontal, ChevronDown, Plus } from "lucide-react";
+import { useState } from "react"
+
+
+
+export default function OptionsBanner () {
+  const [orderBy, setOrderBy] = useState("grid");
+  const [orderShow, setOrderShow] = useState("grid");
+  const [openOrderByMenu, setOpenOrderByMenu] = useState(false);
+
+  return (
+    <div className="w-full flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <div className="flex items-center rounded-full">
+          <button
+            type="button"
+            onClick={() => setOrderShow("grid")}
+            className="cursor-pointer flex items-center gap-2 py-4 px-8 rounded-l-full hover:bg-[var(--hover-bg)] bg-[var(--purple-accent)]/10 text-[var(--purple-accent)]"
+          >
+            <Check className={`${ orderShow === "grid" ? "h-4 w-4" : "hidden" }`} strokeWidth={3}/>
+            <LayoutGrid className="h-4 w-4" strokeWidth={3}/>
+          </button>
+          
+          <button
+            type="button"
+            onClick={() => setOrderShow("line")}
+            className="cursor-pointer flex items-center gap-2 py-4 px-8 rounded-r-full hover:bg-[var(--hover-bg)] bg-[var(--purple-accent)]/10 text-[var(--purple-accent)]"
+          >
+            <Check className={`${ orderShow === "line" ? "h-4 w-4" : "hidden" }`} strokeWidth={3}/>
+            <StretchHorizontal className="h-4 w-4" strokeWidth={3}/>
+          </button>
+        </div>
+
+        <div className="relative">
+          <button
+            type="button"
+            onClick={() => setOpenOrderByMenu(!openOrderByMenu)}
+            className="cursor-pointer py-3 px-6 flex items-center gap-2 rounded-full bg-[var(--hover-bg)] hover:bg-[var(--purple-accent)]/10 text-[var(--purple-accent)]"
+          >
+            <span>Ordenar por</span>
+            <ChevronDown className={`h-4 w-4 ${ openOrderByMenu ? "rotate-180" : "rotate-0"} transition-all duration-300`} strokeWidth={3}/>
+          </button>
+
+          {openOrderByMenu && (
+            <div className="absolute w-full rounded-3xl py-2 flex flex-col items-center gap-2 bg-[var(--hover-bg)] text-[var(--purple-accent)] top-14 left-1/2 -translate-x-1/2">
+              <button
+                type="button"
+                onClick={() => setOrderBy("most-recently")}
+                className="cursor-pointer px-4 py-2 rounded-full hover:bg-[var(--purple-accent)]/10"
+              >
+                Más reciente
+              </button>
+              
+              <button
+                type="button"
+                onClick={() => setOrderBy("title")}
+                className="cursor-pointer px-4 py-2 rounded-full hover:bg-[var(--purple-accent)]/10"
+              >
+                Título
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <button className="text-sm text-black font-semibold flex items-center justify-center gap-2 rounded-full bg-gradient-to-br from-[var(--purple-accent)] to-[var(--sidebar-border)] to-[var(--purple-accent)] hover:bg-gradient-to-br hover:from-[var(--sidebar-border)] to-[var(--purple-accent)]  transition-all duration-300 ease-in-out cursor-pointer py-3 px-6">
+          <Plus className="h-4 w-4" strokeWidth={3}/>
+          <span>Crear cuaderno</span>
+        </button>
+    </div>
+  );
+}
