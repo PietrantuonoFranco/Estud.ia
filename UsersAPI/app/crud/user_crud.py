e5117333f4980cf9244f6c692484b4917a4d8c5a
Franquinios <francopietrantuono999@gmail.com>
2025-12-24 17:26:01 -0300

    feat: add user_crud

---
+++ UsersAPI/app/crud/user_crud.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import Session
+from ..security import hash_password  # Importamos la función de hash_password
+
+from ..schemas.user_schema import UserCreate
+from ..schemas.notebook_schema import NotebookCreate
+
+from ..user_model import User
+from ..notebook_model import Notebook
+
+
+def create_user(db: Session, user: UserCreate):
+    # Encriptamos la contraseña antes de crear el objeto del modelo
+    hashed_pwd = hash_password(user.password)
+    
+    db_user = User(
+        email=user.email,
+        name=user.name,
+        lastname=user.lastname,
+        password=hashed_pwd, # Guardamos el hash, no la clave real
+        profile_image_url=str(user.profile_image_url) if user.profile_image_url else None
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+def get_all_users(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+# --- OPERACIONES DE NOTEBOOK ---
+
+def get_notebooks_by_user(db: Session, user_id: int):
+    return db.query(Notebook).filter(Notebook.users_id == user_id).all()
+
+def create_user_notebook(db: Session, notebook: NotebookCreate):
+    db_notebook = Notebook(**notebook.dict())
+    db.add(db_notebook)
+    db.commit()
+    db.refresh(db_notebook)
+    return db_notebook
+
+# --- OPERACIONES DE ELIMINACIÓN ---
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
