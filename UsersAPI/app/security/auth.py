a4b2e3563dfcdc5079bc353a2f57d72b1f32fab5
Franquinios <francopietrantuono999@gmail.com>
2025-12-30 11:59:09 -0300

    feat: add Cookie implementation and make authenticate_user not async

--- UsersAPI/app/security/auth.py
+++ UsersAPI/app/security/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Cookie, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from ..database import get_db
@@ -7,12 +7,12 @@ from ..models.user_model import User
 
 from ..crud.user_crud import get_user_by_email
 
-from .token import verify_token, oauth2_scheme
+from .token import verify_token
 from .password import verify_password
 
 
 
-async def authenticate_user(db: Session, email: str, password_plana: str):
+def authenticate_user(db: Session, email: str, password_plana: str):
     user = get_user_by_email(db, email)
     if not user:
         return False
@@ -20,10 +20,14 @@ async def authenticate_user(db: Session, email: str, password_plana: str):
         return False
     return user
 
-# 1. Dependencia para obtener el usuario autenticado
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    user = verify_token(token, db) # Función que valida el JWT
-
+# Dependencia para obtener el usuario desde la cookie
+async def get_current_user(accessToken: str = Cookie(alias="accessToken", default=None), db: Session = Depends(get_db)):
+    if not accessToken:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token no encontrado")
+    
+    user = verify_token(accessToken, db)
+    
     if not user:
-        raise HTTPException(status_code=401, detail="Token inválido")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token inválido")
+    
     return user
