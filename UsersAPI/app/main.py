1a3bd9a8e05057a7416898a291dbf40a30bf110f
Franquinios <francopietrantuono999@gmail.com>
2025-12-30 13:29:17 -0300

    feat: add message, summary, flashcard, and quiz models and routers to main application

--- UsersAPI/app/main.py
+++ UsersAPI/app/main.py
@@ -8,15 +8,29 @@ from .routers.user_router import router as users_router
 from .routers.notebook_router import router as notebooks_router
 from .routers.source_router import router as sources_router
 from .routers.auth_router import router as auth_router
+from .routers.message_router import router as messages_router
+from .routers.summary_router import router as summaries_router
+from .routers.flashcard_router import router as flashcards_router
+from .routers.quiz_router import router as quizzes_router
 
 # Models
 from .models.user_model import User
 from .models.notebook_model import Notebook
 from .models.source_model import Source
+from .models.message_model import Message
+from .models.summary_model import Summary
+from .models.flashcard_model import Flashcard
+from .models.quiz_model import Quiz
+from .models.questions_and_answers_model import QuestionsAndAnswers
 
 User.metadata.create_all(bind=engine)
 Notebook.metadata.create_all(bind=engine)
 Source.metadata.create_all(bind=engine)
+Message.metadata.create_all(bind=engine)
+Summary.metadata.create_all(bind=engine)
+Flashcard.metadata.create_all(bind=engine)
+Quiz.metadata.create_all(bind=engine)
+QuestionsAndAnswers.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -38,3 +52,7 @@ app.include_router(users_router)
 app.include_router(notebooks_router)
 app.include_router(sources_router)
 app.include_router(auth_router)
+app.include_router(messages_router)
+app.include_router(summaries_router)
+app.include_router(flashcards_router)
+app.include_router(quizzes_router)
