44110419d44f9046ebd82753d666001d5bae6288
Franquinios <francopietrantuono999@gmail.com>
2025-12-30 00:47:37 -0300

    feat: add migration steps when run UsersAPI

--- UsersAPI/app/main.py
+++ UsersAPI/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 
+from .database import engine
+
 # Routers
 from .routers.user_router import router as users_router
 from .routers.notebook_router import router as notebooks_router
 from .routers.source_router import router as sources_router
+
+# Models
+from .models.user_model import User
+from .models.notebook_model import Notebook
+from .models.source_model import Source
+
+User.metadata.create_all(bind=engine)
+Notebook.metadata.create_all(bind=engine)
+Source.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
