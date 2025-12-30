7732a7310493be9defb249ea77ad55a172c41bfd
Franquinios <francopietrantuono999@gmail.com>
2025-12-30 02:13:36 -0300

    feat: add CORS middleware and configure allowed origins

--- UsersAPI/app/main.py
+++ UsersAPI/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import engine
 
@@ -6,6 +7,7 @@ from .database import engine
 from .routers.user_router import router as users_router
 from .routers.notebook_router import router as notebooks_router
 from .routers.source_router import router as sources_router
+from .routers.auth_router import router as auth_router
 
 # Models
 from .models.user_model import User
@@ -18,6 +20,21 @@ Source.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# 1. Define la lista de orígenes permitidos
+origins = [
+    "http://localhost:3000",
+]
+
+# 2. Agrega el middleware a la aplicación
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Lista de URLs permitidas
+    allow_credentials=True,           # Permite el envío de cookies y tokens
+    allow_methods=["*"],              # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],              # Permite todos los encabezados
+)
+
 app.include_router(users_router)
 app.include_router(notebooks_router)
 app.include_router(sources_router)
+app.include_router(auth_router)
