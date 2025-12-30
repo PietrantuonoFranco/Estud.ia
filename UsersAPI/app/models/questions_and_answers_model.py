9deb12ccbbe2e60259347c9131cf25855bed1863
Franquinios <francopietrantuono999@gmail.com>
2025-12-30 13:25:45 -0300

    feat: add questions_and_answers_model

---
+++ UsersAPI/app/models/questions_and_answers_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+
+class QuestionsAndAnswers(Base):
+    __tablename__ = "questions_and_answers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String(255), nullable=False)
+    answer = Column(String(255), nullable=False)
+    incorrect_answer_1 = Column(String(255), nullable=False)
+    incorrect_answer_2 = Column(String(255), nullable=False)
+    incorrect_answer_3 = Column(String(255), nullable=False)
+    quiz_id = Column(Integer, ForeignKey("quiz.id"), nullable=False)
+
+    quiz = relationship("Quiz", back_populates="questions")
