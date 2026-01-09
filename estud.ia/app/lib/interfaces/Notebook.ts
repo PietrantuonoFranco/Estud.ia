bf817f4589b6a86baa78718c0a60c896ede6da4f
Franquinios <francopietrantuono999@gmail.com>
2026-01-08 21:58:39 -0300

    feat: extend Notebook interface to include messages, summaries, flashcards, and quizzes

--- estud.ia/app/lib/interfaces/Notebook.ts
+++ estud.ia/app/lib/interfaces/Notebook.ts
@@ -1,8 +1,21 @@
+import Message from "./Message";
+import Summary from "./Summary";
+import Flashcard from "./Flashcard";
+import Quiz from "./Quiz";
+
+
 export default interface Notebook {
-    id: number
-    title: string
-    description: string
-    icon: string
-    date: string
-    sourcesCount: number
-  }
+  id: number
+  title: string
+  description: string
+  icon: string
+  date: string
+  sourcesCount: number
+
+  messages: Message[]
+  summaries: Summary[]
+  flashcards: Flashcard[]
+  quizzes: Quiz[]
+
+  user_id: number
+}
