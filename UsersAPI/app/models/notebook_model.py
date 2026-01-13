a5bd610b5b2741bdbdad1173c055bb27282ef922
Franquinios <francopietrantuono999@gmail.com>
2026-01-12 23:03:17 -0300

    feat: increase to 255 title length of notebook

--- UsersAPI/app/models/notebook_model.py
+++ UsersAPI/app/models/notebook_model.py
@@ -7,7 +7,7 @@ class Notebook(Base):
     __tablename__ = "notebooks"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(60), nullable=False)
+    title = Column(String(255), nullable=False)
     icon = Column(String(45), nullable=False)
     description = Column(Text, nullable=False)
     date = Column(Date, nullable=False)
