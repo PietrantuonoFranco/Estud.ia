eb27b83b6925a479e44c89bc242457545a16499e
Franquinios <francopietrantuono999@gmail.com>
2026-01-18 15:43:17 -0300

    fix: correct property names for incorrect answers in QuestionsAndAnswers interface

--- estud.ia/app/lib/interfaces/entities/QuestionsAndAnswers.ts
+++ estud.ia/app/lib/interfaces/entities/QuestionsAndAnswers.ts
@@ -2,8 +2,8 @@ export default interface QuestionsAndAnswers {
     id: number;
     question: string;
     answer: string;
-    incorrect_answers_1: string;
-    incorrect_answers_2: string;
-    incorrect_answers_3: string;
+    incorrect_answer_1: string;
+    incorrect_answer_2: string;
+    incorrect_answer_3: string;
     quiz_id: number;
 }
