6717537de9affb39f269861d29692999a6f2c73d
Franquinios <francopietrantuono999@gmail.com>
2026-01-21 00:30:46 -0300

    feat: create api module with axios instance for API interactions

---
+++ estud.ia/app/lib/api/api.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const API_URL = process.env.API_URL || 'http://localhost:5000';
+
+const api = axios.create({ baseURL: API_URL, withCredentials: true });
+
+export default api;
