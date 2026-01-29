adb59940e585a131f1d09e9a68d16b64d5aa22c1
Franquinios <francopietrantuono999@gmail.com>
2026-01-29 00:08:07 -0300

    refactor: update loading state UI in OptionContainer for improved user experience

--- estud.ia/app/notebook/[id]/components/OptionContainer.tsx
+++ estud.ia/app/notebook/[id]/components/OptionContainer.tsx
@@ -17,18 +17,10 @@ export default function OptionContainer() {
 
   if (isLoading) {
     return (
-      <div className="flex-1 flex flex-col items-center justify-center gap-2">
+      <div className="min-h-[100dvh] flex-1 flex flex-col items-center justify-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="34px" height="34px" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3c4.97 0 9 4.03 9 9"><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"></animateTransform></path></svg>
         <p className="w-full text-center">
-          Generando {
-            option === "flashcards" ? 
-              "tarjetas didácticas..."
-            : 
-              option === "quiz" ?
-              "cuestionario..."
-            :
-              "contenido..."
-          }
+          Generando contenido...
         </p>
       </div>
     );
