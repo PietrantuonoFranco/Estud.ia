dc83cf548c8f22ba65a3ea56826b55dc399d484f
Franquinios <francopietrantuono999@gmail.com>
2026-02-14 20:34:07 -0300

    feat: enhance Header component styles and user avatar display

--- estud.ia/app/components/layout/Header.tsx
+++ estud.ia/app/components/layout/Header.tsx
@@ -33,12 +33,12 @@ export default function Header() {
   }
 
   return (
-    <header className="relative flex h-16 items-center justify-between border-b border-border bg-[var(--panel-bg)] py-3 px-6 z-2000">
+    <header className="relative flex h-16 items-center justify-between border-b border-border bg-[var(--panel-bg)] py-3 px-6 lg:px-42 z-2000">
       <Link href="/" className="flex items-center gap-3">
         <div className="flex h-9 w-9 items-center justify-center rounded-lg">
           <Image  src="/favicon.png" alt="Estud.IA Logo" width={32} height={32} className="rounded-full" />
         </div>
-        <h1 className="hidden md:inline text-lg font-medium text-foreground">Estud.IA</h1>
+        <h1 className="hidden md:inline text-lg font-medium text-foreground pt-1">Estud<span className="text-primary">.IA</span></h1>
       </Link>
 
       <div className="w-full flex sm:hidden items-center justify-end">
@@ -55,22 +55,28 @@ export default function Header() {
         {user && (
           <div className="flex items-center gap-2">
             <div className="ml-6 h-8 w-8">
+              {user.profile_image_url !== null ? (
               <Image src={user.profile_image_url || "/user-avatar.png"} alt="User Avatar" width={32} height={32} className="rounded-full" />
+              ) : (
+                <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary-accent/10 text-sm text-primary font-semibold">
+                  {user.name.charAt(0).toUpperCase()}
+                </div>
+              )}
             </div>
     
-            <div className="text-muted-foreground rounded-full py-1 px-3 bg-[var(--hover-bg)]">{user.name} {user.lastname}</div>
+            <div className="text-muted-foreground rounded-full py-1 px-3 bg-card">{user.name} {user.lastname}</div>
 
             <button
               type="button"
               onClick={handleSettings}          
-              className="cursor-pointer flex items-center gap-2 p-1.5 rounded-full hover:bg-[var(--hover-bg)] bg-[var(--purple-accent)]/10 text-[var(--purple-accent)]"
+              className="cursor-pointer flex items-center gap-2 p-1.5 rounded-full hover:bg-[var(--hover-bg)] bg-card text-foreground"
             >
               <Settings className="h-4 w-4" />
             </button>
             <button
               type="button"
               onClick={handleLogout}
-                className="cursor-pointer p-1.5 rounded-full hover:bg-[var(--hover-bg)] bg-card text-[var(--purple-accent)]"
+                className="cursor-pointer p-1.5 rounded-full hover:bg-hover-bg bg-primary-accent/10 text-primary"
               >
               <LogOut className="h-4 w-4" strokeWidth={2.5}/>
             </button>
@@ -81,14 +87,14 @@ export default function Header() {
           <div className="flex items-center gap-3">
             <Link
               href="/login"
-              className="cursor-pointer flex items-center gap-2 text-muted-foreground rounded-full py-2 px-4 bg-card hover:bg-[var(--hover-bg)]"
+              className="cursor-pointer flex items-center gap-2 text-muted-foreground rounded-lg py-2 px-4 bg-card hover:bg-[var(--hover-bg)]"
             >
               <LogIn  className="h-4 w-4" />
               <span>Iniciar sesión</span>
             </Link>
             <Link
               href="/register"
-              className="cursor-pointer flex items-center gap-2 text-muted-foreground rounded-full py-2 px-4 bg-card hover:bg-[var(--hover-bg)]"
+              className="cursor-pointer flex items-center gap-2 text-muted-foreground rounded-lg py-2 px-4 bg-card hover:bg-[var(--hover-bg)]"
             >
               <UserRoundPlus className="h-4 w-4" />
               <span>Registrarse</span>
